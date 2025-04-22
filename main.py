5e60ed27e44221d72cec343d622970935a341f4e
Preetam Maske <preetamamaske1@gmail.com>
2025-04-21 20:18:07 -0700

    ui changes

--- main.py
+++ main.py
@@ -9,300 +9,171 @@ st.set_page_config(layout="wide")
 
 displayed = []
 
-if 'movie_number' not in st.session_state:
-    st.session_state['movie_number'] = 0
-
-if 'selected_movie_name' not in st.session_state:
-    st.session_state['selected_movie_name'] = ""
-
-if 'user_menu' not in st.session_state:
-    st.session_state['user_menu'] = ""
+# Initialize session state
+st.session_state.setdefault('movie_number', 0)
+st.session_state.setdefault('selected_movie_name', "")
+st.session_state.setdefault('user_menu', "")
 
 
 def main():
     def initial_options():
-        # To display menu
-        st.session_state.user_menu = streamlit_option_menu.option_menu(
-            menu_title='What are you looking for? 👀',
-            options=['Recommend me a similar movie', 'Describe me a movie', 'Check all Movies'],
-            icons=['film', 'film', 'film'],
-            menu_icon='list',
-            orientation="horizontal",
-        )
+        # Vertical menu in the sidebar
+        with st.sidebar:
+            st.markdown("## 🎬 Movie Recommender")
+            st.session_state.user_menu = streamlit_option_menu.option_menu(
+                menu_title=None,  # no header since we have our own markdown
+                options=[
+                    "Recommend me a similar movie",
+                    "Describe me a movie",
+                    "Check all Movies",
+                ],
+                icons=["film", "info-circle", "grid-3x3"],
+                menu_icon="cast",
+                default_index=0,
+                orientation="vertical",
+            )
 
-        if st.session_state.user_menu == 'Recommend me a similar movie':
+        # Dispatch to the right view
+        if st.session_state.user_menu == "Recommend me a similar movie":
             recommend_display()
-
-        elif st.session_state.user_menu == 'Describe me a movie':
+        elif st.session_state.user_menu == "Describe me a movie":
             display_movie_details()
-
-        elif st.session_state.user_menu == 'Check all Movies':
+        else:  # "Check all Movies"
             paging_movies()
 
     def recommend_display():
-
-        st.title('Movie Recommender System')
-
+        st.title("Movie Recommender System")
         selected_movie_name = st.selectbox(
-            'Select a Movie...', new_df['title'].values
+            "Select a Movie...", new_df["title"].values
         )
-
-        rec_button = st.button('Recommend')
-        if rec_button:
+        if st.button("Recommend"):
             st.session_state.selected_movie_name = selected_movie_name
-            recommendation_tags(new_df, selected_movie_name, r'Files/similarity_tags_tags.pkl',"are")
-            recommendation_tags(new_df, selected_movie_name, r'Files/similarity_tags_genres.pkl',"on the basis of genres are")
             recommendation_tags(new_df, selected_movie_name,
-                                r'Files/similarity_tags_tprduction_comp.pkl',"from the same production company are")
-            recommendation_tags(new_df, selected_movie_name, r'Files/similarity_tags_keywords.pkl',"on the basis of keywords are")
-            recommendation_tags(new_df, selected_movie_name, r'Files/similarity_tags_tcast.pkl',"on the basis of cast are")
+                                r"Files/similarity_tags_tags.pkl", "are")
+            recommendation_tags(new_df, selected_movie_name,
+                                r"Files/similarity_tags_genres.pkl", "on the basis of genres are")
+            recommendation_tags(new_df, selected_movie_name,
+                                r"Files/similarity_tags_prduction_comp.pkl", "from the same production company are")
+            recommendation_tags(new_df, selected_movie_name,
+                                r"Files/similarity_tags_keywords.pkl", "on the basis of keywords are")
+            recommendation_tags(new_df, selected_movie_name,
+                                r"Files/similarity_tags_tcast.pkl", "on the basis of cast are")
 
-    def recommendation_tags(new_df, selected_movie_name, pickle_file_path,str):
-
-        movies, posters = preprocess.recommend(new_df, selected_movie_name, pickle_file_path)
-        st.subheader(f'Best Recommendations {str}...')
-
-        rec_movies = []
-        rec_posters = []
-        cnt = 0
-        # Adding only 5 uniques recommendations
-        for i, j in enumerate(movies):
-            if cnt == 5:
+    def recommendation_tags(df, movie_name, pickle_path, desc):
+        movies, posters = preprocess.recommend(df, movie_name, pickle_path)
+        st.subheader(f"Best recommendations {desc}...")
+        rec_movies, rec_posters = [], []
+        for m, p in zip(movies, posters):
+            if len(rec_movies) == 5:
                 break
-            if j not in displayed:
-                rec_movies.append(j)
-                rec_posters.append(posters[i])
-                displayed.append(j)
-                cnt += 1
+            if m not in displayed:
+                rec_movies.append(m)
+                rec_posters.append(p)
+                displayed.append(m)
 
-        # Columns to display informations of movies i.e. movie title and movie poster
-        col1, col2, col3, col4, col5 = st.columns(5)
-        with col1:
-            st.text(rec_movies[0])
-            st.image(rec_posters[0])
-        with col2:
-            st.text(rec_movies[1])
-            st.image(rec_posters[1])
-        with col3:
-            st.text(rec_movies[2])
-            st.image(rec_posters[2])
-        with col4:
-            st.text(rec_movies[3])
-            st.image(rec_posters[3])
-        with col5:
-            st.text(rec_movies[4])
-            st.image(rec_posters[4])
+        cols = st.columns(5)
+        for col, title, poster in zip(cols, rec_movies, rec_posters):
+            with col:
+                st.text(title)
+                st.image(poster)
 
     def display_movie_details():
+        st.title("Movie Details")
 
-        selected_movie_name = st.session_state.selected_movie_name
-        # movie_id = movies[movies['title'] == selected_movie_name]['movie_id']
-        info = preprocess.get_details(selected_movie_name)
+        # Let the user choose which movie to describe
+        movie_name = st.selectbox(
+            "Select a movie to describe:",
+            new_df["title"].values,
+            index=0,
+            key="describe_selectbox"
+        )
+        # Persist selection
+        st.session_state.selected_movie_name = movie_name
+
+        # Now safe to fetch details
+        info = preprocess.get_details(movie_name)
 
         with st.container():
             image_col, text_col = st.columns((1, 2))
             with image_col:
-                st.text('\n')
                 st.image(info[0])
 
             with text_col:
-                st.text('\n')
-                st.text('\n')
-                st.title(selected_movie_name)
-                st.text('\n')
+                st.title(movie_name)
                 col1, col2, col3 = st.columns(3)
                 with col1:
-                    st.text("Rating")
-                    st.write(info[8])
+                    st.metric("Rating", info[8])
                 with col2:
-                    st.text("No. of ratings")
-                    st.write(info[9])
+                    st.metric("No. of ratings", info[9])
                 with col3:
-                    st.text("Runtime")
-                    st.write(info[6])
+                    st.metric("Runtime", f"{info[6]} min")
 
-                st.text('\n')
-                st.write("Overview")
-                st.write(info[3], wrapText=False)
-                st.text('\n')
+                st.markdown("**Overview**")
+                st.write(info[3])
+
                 col1, col2, col3 = st.columns(3)
                 with col1:
-                    st.text("Release Date")
-                    st.text(info[4])
+                    st.markdown(f"**Release Date:** {info[4]}")
                 with col2:
-                    st.text("Budget")
-                    st.text(info[1])
+                    st.markdown(f"**Budget:** ${info[1]:,}")
                 with col3:
-                    st.text("Revenue")
-                    st.text(info[5])
+                    st.markdown(f"**Revenue:** ${info[5]:,}")
 
-                st.text('\n')
                 col1, col2, col3 = st.columns(3)
                 with col1:
-                    str = ""
-                    st.text("Genres")
-                    for i in info[2]:
-                        str = str + i + " . "
-                    st.write(str)
-
+                    st.markdown("**Genres:** " + " · ".join(info[2]))
                 with col2:
-                    str = ""
-                    st.text("Available in")
-                    for i in info[13]:
-                        str = str + i + " . "
-                    st.write(str)
+                    st.markdown("**Available in:** " + " · ".join(info[13]))
                 with col3:
-                    st.text("Directed by")
-                    st.text(info[12][0])
-                st.text('\n')
+                    st.markdown("**Directed by:** " + info[12][0])
 
-        # Displaying information of casts.
-        st.header('Cast')
-        cnt = 0
-        urls = []
-        bio = []
-        for i in info[14]:
-            if cnt == 5:
-                break
-            url, biography= preprocess.fetch_person_details(i)
+        st.header("Cast")
+        urls, bios = [], []
+        for pid in info[14][:5]:
+            url, bio = preprocess.fetch_person_details(pid)
             urls.append(url)
-            bio.append(biography)
-            cnt += 1
+            bios.append(bio)
 
-        col1, col2, col3, col4, col5 = st.columns(5)
-        with col1:
-            st.image(urls[0])
-            # Toggle button to show information of cast.
-            stoggle(
-                "Show More",
-                bio[0],
-            )
-        with col2:
-            st.image(urls[1])
-            stoggle(
-                "Show More",
-                bio[1],
-            )
-        with col3:
-            st.image(urls[2])
-            stoggle(
-                "Show More",
-                bio[2],
-            )
-        with col4:
-            st.image(urls[3])
-            stoggle(
-                "Show More",
-                bio[3],
-            )
-        with col5:
-            st.image(urls[4])
-            stoggle(
-                "Show More",
-                bio[4],
-            )
+        cols = st.columns(5)
+        for col, u, b in zip(cols, urls, bios):
+            with col:
+                st.image(u)
+                stoggle("Show More", b)
 
     def paging_movies():
-        # To create pages functionality using session state.
-        max_pages = movies.shape[0] / 10
-        max_pages = int(max_pages) - 1
+        max_pages = len(movies) // 10
+        col_prev, col_slider, col_next = st.columns([1, 9, 1])
 
-        col1, col2, col3 = st.columns([1, 9, 1])
+        with col_prev:
+            if st.button("Prev") and st.session_state.movie_number >= 10:
+                st.session_state.movie_number -= 10
 
-        with col1:
-            st.text("Previous page")
-            prev_btn = st.button("Prev")
-            if prev_btn:
-                if st.session_state['movie_number'] >= 10:
-                    st.session_state['movie_number'] -= 10
+        with col_slider:
+            pg = st.slider("Page", 0, max_pages, st.session_state.movie_number // 10)
+            st.session_state.movie_number = pg * 10
 
-        with col2:
-            new_page_number = st.slider("Jump to page number", 0, max_pages, st.session_state['movie_number'] // 10)
-            st.session_state['movie_number'] = new_page_number * 10
+        with col_next:
+            if st.button("Next") and st.session_state.movie_number + 10 < len(movies):
+                st.session_state.movie_number += 10
 
-        with col3:
-            st.text("Next page")
-            next_btn = st.button("Next")
-            if next_btn:
-                if st.session_state['movie_number'] + 10 < len(movies):
-                    st.session_state['movie_number'] += 10
-
-        display_all_movies(st.session_state['movie_number'])
+        display_all_movies(st.session_state.movie_number)
 
     def display_all_movies(start):
-
         i = start
-        with st.container():
-            col1, col2, col3, col4, col5 = st.columns(5)
-            with col1:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
+        for _ in range(2):  # two rows
+            cols = st.columns(5)
+            for col in cols:
+                m_id = movies.iloc[i]["movie_id"]
+                poster = preprocess.fetch_posters(m_id)
+                col.image(poster, caption=movies.iloc[i]["title"])
+                i += 1
 
-            with col2:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
-
-            with col3:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
-
-            with col4:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
-
-            with col5:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
-
-        with st.container():
-            col1, col2, col3, col4, col5 = st.columns(5)
-            with col1:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
-
-            with col2:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
-
-            with col3:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
-
-            with col4:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
-
-            with col5:
-                id = movies.iloc[i]['movie_id']
-                link = preprocess.fetch_posters(id)
-                st.image(link, caption=movies['title'][i])
-                i = i + 1
-
-        st.session_state['page_number'] = i
-
+    # Load data & show menu
     with Main() as bot:
         bot.main_()
         new_df, movies, movies2 = bot.getter()
-        initial_options()
+    initial_options()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
